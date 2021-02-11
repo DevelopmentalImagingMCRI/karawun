9090080a17cee02ddd3b3478ca23edc6abcf009a
Richard Beare <Richard.Beare@gmail.com>
2021-02-11 19:18:25 +1100

    numpy version in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='richard.beare@mcri.edu.au',
     description=('DICOM image, segmgmentation image and fibre object converter'),
     long_description=(LONG_DESCRIPTION),
-    install_requires=["numpy==1.13.0",
+    install_requires=["numpy>=1.13.0",
                       "pydicom==1.3",
                       "SimpleITK==1.2.0"],
     entry_points={'console_scripts': ['importTractography = karawun.commandline:import_tractography_cl']}
